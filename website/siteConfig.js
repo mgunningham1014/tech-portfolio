31c659a2716a0b0cd8331780ebe1d02c82bc3b11
Mandi Gunningham <59667573+mgunningham1014@users.noreply.github.com>
2020-10-02 15:18:11 -0400

    Update siteConfig.js
    Added personal info

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Mandi Gunningham', // Title for your website.
+    tagline: 'Always curious. Always learning. Always having fun!',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://mgunningham1014.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'https://www.linkedin.com/in/mandigunningham/',
         twitter: '',
-        github: '',
+        github: 'https://www.github.com/mgunningham1014/',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
